7f1f012ff9040b5778fcf1b43d3058c61150b1d3
Gabriel <odakura.gabriel@gmail.com>
2024-04-19 20:31:36 -0300

    Better version of the program

--- src/Main.java
+++ src/Main.java
@@ -9,34 +9,16 @@ public class Main {
         try {
             String path = args[0] + "\\" + args[1];
             FileReader input = new FileReader(path);
-            StringBuilder builder = new StringBuilder();
             BufferedReader reader = new BufferedReader(input);
 
-            //lê os tokens
-            String lines;
-            while ((lines = reader.readLine()) != null){
-                builder.append(lines);
-                System.out.println(lines);
-            }
-
-            //fecha o buffer para não ter entrada a mais de dados
-            reader.close();
-
-            //transformando a string recebida em uma array de caracteres
-            String transform = builder.toString();
-            char[] cadeia = null;
-            cadeia = new char[transform.length()];
-            for(int i = 0; i < transform.length(); i++){
-                cadeia[i] = transform.charAt(i);
-            }
-
-
+            //análise Léxica
+            int caracterAtual;
             StringBuilder tokenBuilder = new StringBuilder();
             ArrayList<Token> tokenList = new ArrayList<>();
-            for (int i = 0; i < cadeia.length; i++){
-                System.out.println("Char: " + cadeia[i] + "\tInt: " + (int) cadeia[i] + "\tAt: " + i);
-                if (cadeia[i] != 32 && cadeia[i] != '\n'){
-                    tokenBuilder.append(cadeia[i]);
+            while ((caracterAtual = reader.read()) != -1){
+                System.out.println(tokenBuilder.toString());
+                if (caracterAtual != 32 && caracterAtual != '\n' && caracterAtual != '\r'){
+                    tokenBuilder.append((char) caracterAtual);
                 }else{
                     if(!tokenBuilder.isEmpty()){
                         Token newToken = new Token(TokenType.id, tokenBuilder.toString().toCharArray());
@@ -44,15 +26,20 @@ public class Main {
                         tokenList.add(newToken);
                     }
                 }
-                if(i == (cadeia.length - 1)){
-                    Token newToken = new Token(TokenType.id, tokenBuilder.toString().toCharArray());
-                    tokenBuilder.setLength(0);
-                    tokenList.add(newToken);
-                }
             }
 
+            if(caracterAtual == -1){
+                Token newToken = new Token(TokenType.id, tokenBuilder.toString().toCharArray());
+                tokenBuilder.setLength(0);
+                tokenList.add(newToken);
+            }
+
+            //fecha o buffer para não ter entrada a mais de dados
+            reader.close();
+
+            //debug function
+            System.out.println("tamanho: " + tokenList.size());
             for (int i = 0; i < tokenList.size(); i++){
-                System.out.println("tamanho: " + tokenList.size());
                 System.out.println(tokenList.get(i));
             }
 
